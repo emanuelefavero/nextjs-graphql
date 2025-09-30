81f0f2bb4f6dbe76b633770b5a65cd2874250f91
Emanuele Favero <emafaux89@gmail.com>
2025-09-30 16:26:37 +0200

    feat: add CountriesLoader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,13 @@
-import { client } from '@/lib/graphql-client'
-import { COUNTRIES_QUERY } from '@/lib/queries'
-import type { Countries, Country } from '@/types/countries'
-
-// TODO Load data inside a loader component and put it inside a Suspense
+import CountriesLoader from '@/components/Countries/CountriesLoader'
+import { Suspense } from 'react'
 
 export default async function Home() {
-  const data = await client.request<Countries>(COUNTRIES_QUERY)
-
   return (
     <>
       <h1>Next.js GraphQL</h1>
-
-      <ul>
-        {data.countries.slice(0, 10).map((country: Country) => (
-          <li key={country.code}>
-            {country.emoji} {country.name} ({country.code})
-          </li>
-        ))}
-      </ul>
+      <Suspense fallback={<div>Loading countries...</div>}>
+        <CountriesLoader />
+      </Suspense>
     </>
   )
 }
---
+++ src/components/Countries/CountriesLoader.tsx
@@ -0,0 +1,19 @@
+import { client } from '@/lib/graphql-client'
+import { COUNTRIES_QUERY } from '@/lib/queries'
+import type { Countries, Country } from '@/types/countries'
+
+export default async function CountriesLoader() {
+  const data = await client.request<Countries>(COUNTRIES_QUERY)
+
+  if (data.countries.length === 0) return <div>No countries found</div>
+
+  return (
+    <ul>
+      {data.countries.slice(0, 10).map((country: Country) => (
+        <li key={country.code}>
+          {country.emoji} {country.name} ({country.code})
+        </li>
+      ))}
+    </ul>
+  )
+}
